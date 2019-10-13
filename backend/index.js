4d4292ea46c3666714ccac172ec2089b02da3f39
Mauricio Lima <github@mauriciolima.eti.br>
2019-10-13 18:33:20 -0300

    Server reading for static files

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,45 @@
 const http = require('http')
+const fs   = require('fs')
 
 const PORT = 3000
 
-http.createServer( (request,response) => {
+const contents = (path) => {
+    return new Promise( (resolve,reject) => {
+        console.log('path : ', path)
+
+        fs.readFile('../frontend' + path, (err, contents) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(contents)
+        })
+    })
+}
+
+http.createServer( async (request,response) => {
     response.writeHead(200, { 'Content-type' : 'text/html' })
 
     console.log(`method : ${request.method}`)
     console.log(`url    : ${request.url}`   )
     console.log()
 
-    response.write('result')
+    try
+    {
+        let path
+
+        path = request.url
+        if (request.url == '/')
+        {
+            path = '/index.html'
+        }
+        const content = await contents(path)
+        response.write(content)
+    }
+    catch (error)
+    {
+        response.write('error : ' + error)
+    }
     response.end()
 
 }).listen(PORT, () => {
