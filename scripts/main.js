0149572390e2d761b550795a59fbb7a0f7e00f9d
Mauricio Lima <github@mauriciolima.eti.br>
2019-10-13 19:50:55 -0300

    Audio record controls

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,57 @@
 ( () => {
+    let   mediaRecorder
+    const recordedChunks = [];
 
-    function DOMLoaded()
+    async function DOMLoaded()
     {
-        alert('DOM Loaded')
+        const recordStart = document.getElementById('recordStart')
+        if (recordStart) {
+            recordStart.addEventListener('click', mediaRecorderStart)
+        }
+
+        const recordStop = document.getElementById('recordStop')
+        if (recordStop) {
+            recordStop.addEventListener('click', mediaRecorderStop)
+        }
+
+        const player = document.getElementById('player')
+        if (!player) return
+
+        const source = await navigator.mediaDevices.getUserMedia({ audio : true, video : false })
+        if (window.URL)
+        {
+            player.srcObject = source
+            return
+        }
+        player.src = source
+    }
+
+    mediaRecorder  = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+    if (mediaRecorder)
+    {
+        mediaRecorder.addEventListener('dataavailable', mediaRecorderData)
+    }
+
+    function mediaRecorderStart()
+    {
+        mediaRecorder.start();
+    }
+
+    function mediaRecorderStop()
+    {
+        mediaRecorder.stop();
+    }
+
+    function mediaRecorderData(e)
+    {
+        if (e.data.size > 0) {
+            recordedChunks.push(e.data);
+          }
+    
+          if(shouldStop === true && stopped === false) {
+            mediaRecorder.stop();
+            stopped = true;
+          }
     }
 
     document.addEventListener('DOMContentLoaded', DOMLoaded)
