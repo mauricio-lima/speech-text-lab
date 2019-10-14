e22767bf5a1ad9fb0461e65b3d08d62c3ff56bc7
Mauricio Lima <github@mauriciolima.eti.br>
2019-10-13 22:02:32 -0300

    update synced files

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,55 @@
 ( () => {
     let   mediaRecorder
+    let   source
     const recordedChunks = [];
 
     async function DOMLoaded()
     {
         const recordStart = document.getElementById('recordStart')
         if (recordStart) {
-            recordStart.addEventListener('click', mediaRecorderStart)
+            recordStart.addEventListener('click', () => {
+                mediaRecorder.start();
+            })
         }
 
         const recordStop = document.getElementById('recordStop')
         if (recordStop) {
-            recordStop.addEventListener('click', mediaRecorderStop)
+            recordStop.addEventListener('click', () => {
+                mediaRecorder.stop();
+            })
         }
 
         const player = document.getElementById('player')
         if (!player) return
 
-        const source = await navigator.mediaDevices.getUserMedia({ audio : true, video : false })
-        if (window.URL)
+        source = await navigator.mediaDevices.getUserMedia({ audio : true, video : false })
+
+
+        mediaRecorder  = new MediaRecorder(source, {mimeType: 'audio/webm'});
+        if (mediaRecorder)
         {
-            player.srcObject = source
-            return
+            mediaRecorder.addEventListener('dataavailable', mediaRecorderData)
+
+            mediaRecorder.addEventListener('stop',          mediaRecorderStop)
         }
-        player.src = source
     }
 
-    mediaRecorder  = new MediaRecorder(stream, {mimeType: 'audio/webm'});
-    if (mediaRecorder)
-    {
-        mediaRecorder.addEventListener('dataavailable', mediaRecorderData)
-    }
-
-    function mediaRecorderStart()
-    {
-        mediaRecorder.start();
-    }
-
-    function mediaRecorderStop()
-    {
-        mediaRecorder.stop();
-    }
 
     function mediaRecorderData(e)
     {
         if (e.data.size > 0) {
             recordedChunks.push(e.data);
           }
-    
-          if(shouldStop === true && stopped === false) {
-            mediaRecorder.stop();
-            stopped = true;
-          }
+    }
+
+
+    function mediaRecorderStop()
+    {
+        const source = URL.createObjectURL(new Blob(recordedChunks))
+        const player = document.getElementById('player')
+        if (!player) return
+
+        player.src = source
     }
 
     document.addEventListener('DOMContentLoaded', DOMLoaded)
